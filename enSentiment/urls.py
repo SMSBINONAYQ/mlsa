dc29f39bee05fa4442b7e6c00c464084f3a85dc9
SMSBINONAYQ <abomnsoor3@gmail.com>
2022-05-03 06:11:48 +0300

    init commit

---
+++ enSentiment/urls.py
@@ -0,0 +1,19 @@
+from . import views
+from django.urls import path, include,re_path
+
+app_name = 'enSentiment'
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login',views.login, name='login'),
+    path('register',views.register, name='register'),
+    path('logout',views.logout, name='logout'),
+    path('result',views.result, name='result'),
+    re_path(r'^result/(?P<id>\d+)/$', views.result ,name='result'),
+    re_path(r'^del_result/(?P<id>\d+)/$', views.del_result ,name='del_result'),
+    path('myresults',views.myresults, name='myresults'),
+    path('profile',views.profile, name='profile'),
+    path('aboutus',views.aboutus, name='aboutus'),
+    path('ajax/getAnalysis',views.getAnalysis, name='getAnalysis'),
+]
