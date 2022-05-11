49f6873b1368adfd78faff6c8addf9ae08cef879
SMSBINONAYQ <abomnsoor3@gmail.com>
2022-05-12 00:22:35 +0300

    add domain in Allowed hosts

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure--n8k&pku*8o-9mlp#^x25_bm%eqhb80r+w7_=&1!as0raa9uyq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['164.92.155.70']
+ALLOWED_HOSTS = ['164.92.155.70', 'www.mlsa.cloud', 'mlsa.cloud']
 
 SESSION_COOKIE_AGE = 86400 #After 24 hours  
 
